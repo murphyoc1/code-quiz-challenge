d3a46a15b1e1b35856c23bf274c547e6f9500f84
murphyoc1 <murphyoc1@gmail.com>
2021-09-19 20:37:44 -0400

    added pseudo code

---
+++ assets/script.js
@@ -0,0 +1,76 @@
+var quizContainer = document.getElementById('quiz');
+var submitContainer = document.getElementById('submit')
+var resultsContainer = document.getElementById('results')
+
+
+// array section of questions to ask 
+
+let questions = [
+    {
+        question: "question 1?",
+        answers: {
+        a: "a",
+        b: "b",
+        c: "c"
+        },
+        correctAnswer : "a"
+    },
+    {
+        question: "question 2?",
+        answers: {
+        a: "a",
+        b: "b",
+        c: "c"
+        },
+        correctAnswer : "b"
+    },
+    {
+        question: "question 3?",
+        answers: {
+        a: "a",
+        b: "b",
+        c: "c"
+        },
+        correctAnswer : "c"
+    }
+];
+
+// quiz making function 
+function makeQuiz() {
+    var output =[];
+
+    //want to call the questions array to fill in the quiz
+    questions.forEach(
+    
+        // add code where you list our the possible answers as a button 
+        //to select in the html browser. This is what will bring the array question
+        //out to the user to attempt to take the quiz.
+    );
+}
+
+// add a loop to go through the questions here
+questions.forEach(()) {});
+
+// add a loop for collecting the correct answers using a 
+answers.push()
+
+//run the funciton 
+makeQuiz();
+
+
+// make the results function for how the quiz went
+function quizResults () {
+
+    // add variables to follow correct answers
+    ver answerCorrect = querySelectorAll('.correctAnswer');
+    var numCorrect = 0;
+
+    // add for loop and if statement 
+    if( answer correct){
+        numCorrect++;
+    }
+};
+
+// run quizResults
+
+quizResults();
